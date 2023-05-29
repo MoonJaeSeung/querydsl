1ca61c2e6957e18a724008775157e7f595451e5a
MoonJaeSeung <wotmdskfk32@naver.com>
2023-05-29 21:39:25 +0900

    초기 세팅

--- src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -1,10 +1,20 @@
 package study.querydsl;
 
+import com.sun.xml.internal.ws.api.model.wsdl.editable.EditableWSDLMessage;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
 
 @SpringBootTest
+@Transactional
 class QuerydslApplicationTests {
+
+	@Autowired
+	EntityManager em;
+
 
 	@Test
 	void contextLoads() {
