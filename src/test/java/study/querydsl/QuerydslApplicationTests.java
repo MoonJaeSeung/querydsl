1bb63e4bc942d0759dd07658cc44160517593024
MoonJaeSeung <wotmdskfk32@naver.com>
2023-05-29 23:13:32 +0900

    기본 Q-Type 활용

--- src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -1,23 +1,41 @@
 package study.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.sun.xml.internal.ws.api.model.wsdl.editable.EditableWSDLMessage;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Hello;
+import study.querydsl.entity.QHello;
 
 import javax.persistence.EntityManager;
 
+import static org.assertj.core.api.Assertions.*;
+
 @SpringBootTest
 @Transactional
+@Commit
 class QuerydslApplicationTests {
 
-	@Autowired
-	EntityManager em;
+    @Autowired
+    EntityManager em;
 
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+        Hello hello = new Hello();
+        em.persist(hello);
 
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        QHello qHello = QHello.hello; //Querydsl Q타입 동작 확인
+
+        Hello result = query
+                .selectFrom(qHello)
+                .fetchOne();
+        Assertions.assertThat(result).isEqualTo(hello);
+        Assertions.assertThat(result.getId()).isEqualTo(hello.getId());
+    }
 }
